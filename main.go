bcf4c30d8743ef0ad8a36ee649c96c8da7548f25
preyaswi <preyaswiprakashan@gmail.com>
2023-11-14 05:38:16 +0530

    http changed to https

--- main.go
+++ main.go
@@ -36,7 +36,7 @@ func main() {
 	docs.SwaggerInfo.Version = "1.0"
 	docs.SwaggerInfo.Host = "localhost:8080"
 	docs.SwaggerInfo.BasePath = ""
-	docs.SwaggerInfo.Schemes = []string{"http"}
+	docs.SwaggerInfo.Schemes = []string{"https"}
 	cfg, err := config.LoadConfig()
 	if err != nil {
 		log.Fatalf("error loading the config file")
